e6ec94c16a9b474998f43bcab0a5f5ffc6c23d7b
Raquel <raquel.queiroz@youse.com.br>
2017-07-04 19:50:50 -0300

    fix bug

--- app/controllers/pull_requests_links_controller.rb
+++ app/controllers/pull_requests_links_controller.rb
@@ -16,20 +16,23 @@ class PullRequestsLinksController < ::ApplicationController
     pull_requests_link = params[:text]
     match = pull_requests_link.match(/.*(github.com\/youse-seguradora\/.*\/pull\/\d*)/)
     pull_request = PullRequestsLink.find_by(link: match[1])
-    response = {
-      response_type: 'in_channel',
-      text: "Você já mandou #{pull_request.count}x o link desse PR nesse canal!",
-      attachments: [
-        {
-        text: "E foi criado em: #{pull_request.created_at}"
-        }
-      ]
-    }
 
     if pull_request
+      response = {
+        response_type: 'in_channel',
+        text: "Você já mandou #{pull_request.count}x o link desse PR nesse canal!",
+        attachments: [
+          {
+          text: "E foi mandado a primeira vez em: #{pull_request.created_at.strftime('%m/%d/%Y as %I:%M%p')}"
+          }
+        ]
+      }
+
       render status: 200, json: response
     else
-      render status: 204
+      response = { response_type: 'in_channel', text: "Você já mandou 0x o link desse PR nesse canal!" }
+
+      render status: 204, json: response
     end
   end
 end
